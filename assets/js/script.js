b00231a375a875e97c6d61a74fc94722ad3263b0
Diarmuid Sheeran <dssheeran@outlook.com>
2023-04-25 19:45:16 +0000

    add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    }
+    else if (gameType === "subtract") {
+        dipslaySubtractQuestion(num1, num2);
     }else {
         alert(`Unknown game type: ${gametype}`);
         trow `Unknown gametype: ${gameType}, Aborting!`;
@@ -73,7 +76,10 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } 
+    else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    }else {
         alert(`Unimplimented operator ${operator}`);
         throw `Unimplimented operator ${operator}, Aborting!`
     }
@@ -104,7 +110,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function dipslaySubtractQuestion() {
+function dipslaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 
 }
 
